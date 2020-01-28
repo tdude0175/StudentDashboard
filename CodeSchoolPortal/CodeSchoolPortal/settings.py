3828b92883a48e33cf347c8794fe625a663ce413
autumn-ragland <raglanda998@gmail.com>
2020-01-28 10:50:54 -0600

    move whitenoise

--- CodeSchoolPortal/CodeSchoolPortal/settings.py
+++ CodeSchoolPortal/CodeSchoolPortal/settings.py
@@ -42,8 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # https://warehouse.python.org/project/whitenoise/
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
