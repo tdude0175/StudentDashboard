ae4c7768aea2c67fb65b86b77740336812c4a207
autumn-ragland <raglanda998@gmail.com>
2020-01-28 09:48:15 -0600

    migrate sqlite to postgresql

--- CodeSchoolPortal/CodeSchoolPortal/settings.py
+++ CodeSchoolPortal/CodeSchoolPortal/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'CodeSchoolPortal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'portaldb',
+        'USER': 'student',
+        'PASSWORD': 'C0d3Cr3w',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
